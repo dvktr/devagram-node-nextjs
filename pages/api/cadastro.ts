cde351b98bbda8fcb060b1704ce43d06a786aa09
dvktr <victormendes04012004@gmail.com>
2022-04-21 14:49:20 -0300

    validacao de extensao

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -13,44 +13,50 @@ const handler = nc()
     req: NextApiRequest,
     res: NextApiResponse<RespostaPadraoMsg>
   ) => {
-    const usuario = req.body as CadastroRequisicao;
+    try {
+      const usuario = req.body as CadastroRequisicao;
 
-    if (!usuario.nome || usuario.nome.length < 2) {
-      return res.status(400).json({ erro: 'nome invalido' })
+      if (!usuario.nome || usuario.nome.length < 2) {
+        return res.status(400).json({ erro: 'nome invalido' })
+      }
+
+      if (!usuario.email || usuario.email.length < 2 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
+        return res.status(400).json({ erro: 'email invalido' })
+      }
+
+      if (!usuario.senha || usuario.senha.length < 3) {
+        return res.status(400).json({ erro: 'senha invalido' })
+      }
+
+      //validacao de duplucidade de email
+      const usuariComMesmoEmail = await UsuarioModel.find({ email: usuario.email })
+      if (usuariComMesmoEmail && usuariComMesmoEmail.length > 0) {
+        return res.status(400).json({ erro: 'email ja cadastrado' })
+      }
+
+      //enviar a imagem do multer para o cosmic
+      const image = await uploadImagemCosmic(req);
+
+      //salvar usuario
+      const usuarioAoSerSalvo = {
+        nome: usuario.nome,
+        email: usuario.email,
+        senha: md5(usuario.senha),
+        avatar: image?.media?.url
+      }
+      await UsuarioModel.create(usuarioAoSerSalvo);
+      return res.status(200).json({ msg: "Usuario cadastrado com sucesso" })
+  } catch (e: any) {
+      console.log(e);
+      return res.status(400).json({ erro: e.toString() })
     }
 
-    if (!usuario.email || usuario.email.length < 2 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
-      return res.status(400).json({ erro: 'email invalido' })
-    }
-
-    if (!usuario.senha || usuario.senha.length < 3) {
-      return res.status(400).json({ erro: 'senha invalido' })
-    }
-
-    //validacao de duplucidade de email
-    const usuariComMesmoEmail = await UsuarioModel.find({ email: usuario.email })
-    if (usuariComMesmoEmail && usuariComMesmoEmail.length > 0) {
-      return res.status(400).json({ erro: 'email ja cadastrado' })
-    }
-
-    //enviar a imagem do multer para o cosmic
-    const image = await uploadImagemCosmic(req);
-
-    //salvar usuario
-    const usuarioAoSerSalvo = {
-      nome: usuario.nome,
-      email: usuario.email,
-      senha: md5(usuario.senha),
-      avatar: image?.media?.url
-    }
-    await UsuarioModel.create(usuarioAoSerSalvo);
-    return res.status(200).json({ msg: "Usuario cadastrado com sucesso" })
   });
 
-  export const config = {
-    api: {
-      bodyParser : false
-    }
+export const config = {
+  api: {
+    bodyParser: false
   }
+}
 
 export default conectarMongoDB(handler);
